4f39e5b847047b2c8eac931de133c53e259c84c8
chiki-1121 <tr.giraffe@gmail.com>
2023-10-24 09:29:51 +0900

    telリンク

--- src/js/script.js
+++ src/js/script.js
@@ -1,32 +1,45 @@
-
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
-//ドロワーメニュー
-$(".js-hamburger").click(function () {
-    if ($(".js-hamburger").hasClass('is-active')) {
-        $(".js-hamburger").removeClass("is-active");
-        $("body").removeClass("is-active");
-        $(".js-sp-nav").fadeOut(300);
-    } else {
-        $(".js-hamburger").addClass("is-active");
-        $("body").addClass("is-active");
-        $(".js-sp-nav").fadeIn(300);
-    };
+    //ドロワーメニュー
+    $(".js-hamburger").click(function () {
+        if ($(".js-hamburger").hasClass('is-active')) {
+            $(".js-hamburger").removeClass("is-active");
+            $("body").removeClass("is-active");
+            $(".js-sp-nav").fadeOut(300);
+        } else {
+            $(".js-hamburger").addClass("is-active");
+            $("body").addClass("is-active");
+            $(".js-sp-nav").fadeIn(300);
+        };
+    });
+
+    // トップへ戻るボタン
+    let topBtn = $('.to-top');
+    topBtn.hide();
+
+    // ボタンの表示設定
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 70) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+
+    
+
 });
+//767以下のみtelリンク
+var telLink = document.getElementById('telLink');
 
-// トップへ戻るボタン
-let topBtn = $('.to-top');
-topBtn.hide();
-
-// ボタンの表示設定
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 70) {
-        topBtn.fadeIn();
+// 画面幅に基づいて href 属性を設定
+function setHrefBasedOnScreenWidth() {
+    if (window.innerWidth >= 768) {
+        telLink.href = "#footer";
     } else {
-        topBtn.fadeOut();
+        telLink.href = "tel:0123456789";
     }
-});
+}
 
-
-
-
-});
+// 初期設定と画面サイズ変更時に設定を更新
+setHrefBasedOnScreenWidth();
+window.addEventListener('resize', setHrefBasedOnScreenWidth);
